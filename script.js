8f076a7b5e5ff924fd22c387d5ff59491359c727
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-08-07 22:16:00 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let username = "sameera";
+
+console.log(`welcome ${username}`);
+
+function service_charge(total) {
+  return total * 0.1;
+}
+
+console.log(`Service Charge : ${service_charge(2270)}`);
